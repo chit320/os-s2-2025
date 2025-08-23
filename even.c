2efdebf4dc9cb1581e929098f7100d82bb5b10be
chit320 <hichitshing320@gmail.com>
2025-08-24 01:32:56 +0930

    test

--- even.c
+++ even.c
@@ -1,55 +1,64 @@
-#define _POSIX_C_SOURCE 200809L
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <signal.h>
 #include <errno.h>
 
-static void hup_handler(int sig) {
+static void hup_handler(int sig)
+{
     (void)sig;
     const char msg[] = "Ouch!\n";
     write(STDOUT_FILENO, msg, sizeof(msg) - 1);
 }
-static void int_handler(int sig) {
+static void int_handler(int sig)
+{
     (void)sig;
     const char msg[] = "Yeah!\n";
     write(STDOUT_FILENO, msg, sizeof(msg) - 1);
 }
 
-static int install_handler(int signo, void (*fn)(int)) {
+static int install_handler(int signo, void (*fn)(int))
+{
     struct sigaction sa;
     sa.sa_handler = fn;
     sigemptyset(&sa.sa_mask);
-    sa.sa_flags = SA_RESTART;    
+    sa.sa_flags = SA_RESTART;
     return sigaction(signo, &sa, NULL);
 }
 
-int main(int argc, char *argv[]) {
-    if (argc != 2) {
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+    {
         fprintf(stderr, "Usage: %s <n>\n", argv[0]);
         return 1;
     }
     char *endp = NULL;
     long n = strtol(argv[1], &endp, 10);
-    if (!endp || *endp != '\0' || n < 0) {
+    if (!endp || *endp != '\0' || n < 0)
+    {
         fprintf(stderr, "Invalid n: %s\n", argv[1]);
         return 1;
     }
 
-    if (install_handler(SIGHUP, hup_handler) == -1) {
+    if (install_handler(SIGHUP, hup_handler) == -1)
+    {
         perror("sigaction SIGHUP");
         return 1;
     }
-    if (install_handler(SIGINT, int_handler) == -1) {
+    if (install_handler(SIGINT, int_handler) == -1)
+    {
         perror("sigaction SIGINT");
         return 1;
     }
 
-    for (long i = 0; i < n; i++) {
+    for (long i = 0; i < n; i++)
+    {
         printf("%ld\n", 2 * i);
         fflush(stdout);
         unsigned int remain = 5;
-        while ((remain = sleep(remain)) != 0) {
+        while ((remain = sleep(remain)) != 0)
+        {
         }
     }
     return 0;
--- minishell.c
+++ minishell.c
@@ -16,74 +16,139 @@
 #include <stdlib.h>
 #include <signal.h>
 
-#define NV 20			/* max number of command tokens */
-#define NL 100			/* input buffer size */
-char            line[NL];	/* command input buffer */
+#define NV 20  /* max number of command tokens */
+#define NL 100 /* input buffer size */
+char line[NL]; /* command input buffer */
+#define MAXJ 64
 
+struct job
+{
+  int id;
+  pid_t pid;
+  int active;
+  char cmdline[NL];
+};
 
-/*
-	shell prompt
- */
+static struct job jobs[MAXJ];
+static int next_job_id = 1;
+
+static void build_cmdline(char *dst, size_t sz, char *const argvv[])
+{
+  size_t pos = 0;
+  for (int i = 0; argvv[i]; i++)
+  {
+    if (i && pos < sz - 1)
+      dst[pos++] = ' ';
+    for (const char *p = argvv[i]; *p && pos < sz - 1; ++p)
+      dst[pos++] = *p;
+    if (pos >= sz - 1)
+      break;
+  }
+  dst[pos] = '\0';
+}
+
+static void add_job(pid_t pid, const char *cmdline)
+{
+  for (int i = 0; i < MAXJ; i++)
+  {
+    if (!jobs[i].active)
+    {
+      jobs[i].active = 1;
+      jobs[i].pid = pid;
+      jobs[i].id = next_job_id++;
+      if (next_job_id <= 0)
+        next_job_id = 1;
+      snprintf(jobs[i].cmdline, sizeof(jobs[i].cmdline), "%s", cmdline);
+      printf("[%d] %d\n", jobs[i].id, (int)pid);
+      fflush(stdout);
+      return;
+    }
+  }
+  fprintf(stderr, "Jobs table full; not tracking pid %d\n", (int)pid);
+}
+
+static void reap_background(void)
+{
+  int status;
+  pid_t p;
+  while ((p = waitpid(-1, &status, WNOHANG)) > 0)
+  {
+    for (int i = 0; i < MAXJ; i++)
+    {
+      if (jobs[i].active && jobs[i].pid == p)
+      {
+        jobs[i].active = 0;
+        printf("[%d]+ Done                 %s\n", jobs[i].id, jobs[i].cmdline);
+        fflush(stdout);
+        break;
+      }
+    }
+  }
+}
 
 void prompt(void)
 {
-  // ## REMOVE THIS 'fprintf' STATEMENT BEFORE SUBMISSION
   fprintf(stdout, "\n msh> ");
   fflush(stdout);
 }
-
 
 /* argk - number of arguments */
 /* argv - argument vector from command line */
 /* envp - environment pointer */
 int main(int argk, char *argv[], char *envp[])
 {
-   int             frkRtnVal;	    /* value returned by fork sys call */
-  char           *v[NV];	        /* array of pointers to command line tokens */
-  char           *sep = " \t\n";  /* command line token separators    */
-  int             i;		          /* parse index */
+  int frkRtnVal;       /* value returned by fork sys call */
+  char *v[NV];         /* array of pointers to command line tokens */
+  char *sep = " \t\n"; /* command line token separators    */
+  int i;               /* parse index */
 
-    /* prompt for and process one command line at a time  */
+  /* prompt for and process one command line at a time  */
 
-  while (1) {			/* do Forever */
+  while (1)
+  { /* do Forever */
     prompt();
     fgets(line, NL, stdin);
     fflush(stdin);
 
     // This if() required for gradescope
-    if (feof(stdin)) {		/* non-zero on EOF  */
+    if (feof(stdin))
+    { /* non-zero on EOF  */
       exit(0);
     }
-    if (line[0] == '#' || line[0] == '\n' || line[0] == '\000'){
-      continue;			/* to prompt */
+    if (line[0] == '#' || line[0] == '\n' || line[0] == '\000')
+    {
+      continue; /* to prompt */
     }
 
     v[0] = strtok(line, sep);
-    for (i = 1; i < NV; i++) {
+    for (i = 1; i < NV; i++)
+    {
       v[i] = strtok(NULL, sep);
-      if (v[i] == NULL){
-	      break;
+      if (v[i] == NULL)
+      {
+        break;
       }
     }
     /* assert i is number of tokens + 1 */
 
     /* fork a child process to exec the command in v[0] */
-    switch (frkRtnVal = fork()) {
-      case -1:			/* fork returns error to parent process */
-      {
-	      break;
-      }
-      case 0:			/* code executed only by child process */
-      {
-	      execvp(v[0], v);
-      }
-      default:			/* code executed only by parent process */
-      {
-      	wait(0);
-        // REMOVE PRINTF STATEMENT BEFORE SUBMISSION
-        printf("%s done \n", v[0]);
-    	  break;
-      }
-    }				/* switch */
-  }				/* while */
-}				/* main */
+    switch (frkRtnVal = fork())
+    {
+    case -1: /* fork returns error to parent process */
+    {
+      break;
+    }
+    case 0: /* code executed only by child process */
+    {
+      execvp(v[0], v);
+    }
+    default: /* code executed only by parent process */
+    {
+      wait(0);
+      // REMOVE PRINTF STATEMENT BEFORE SUBMISSION
+      printf("%s done \n", v[0]);
+      break;
+    }
+    } /* switch */
+  } /* while */
+} /* main */
